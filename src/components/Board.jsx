6fb5540987b83da34fca5e8c7e823336913451f7
Ezra Joffe-Hancock <ewj1@williams.edu>
2025-09-16 09:52:03 -0600

    working implementation (random configuration of cells at start)

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,9 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 
 export function Board({ board, showGrid }) {
   const numCols = board[0].length;
   const numRows = board.length;
-  const resolution = 20;
+  const resolution = 3;
   const strokeWidth = 1;
   const canvasRef = useRef(null);
 
@@ -22,29 +22,32 @@ export function Board({ board, showGrid }) {
     ctx.scale(dpr, dpr);
     ctx.imageSmoothingEnabled = false;
 
-    //erase
-    ctx.clearRect(0, 0, displayWidth, displayHeight);
+    function draw() {
+      //erase
+      ctx.clearRect(0, 0, displayWidth, displayHeight);
 
-    //draw cells
-    for (let i = 0; i < numRows; i++) {
-      for (let j = 0; j < numCols; j++) {
-        ctx.fillStyle = board[i][j] ? "black" : "white";
+      //draw cells
+      for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numCols; j++) {
+          ctx.fillStyle = board[i][j] ? "black" : "white";
 
-        ctx.fillRect(
-          Math.floor(strokeWidth + resolution * i),
-          Math.floor(strokeWidth + resolution * j),
-          resolution,
-          resolution
-        );
-
-        // ctx.strokeRect(
-        //   Math.floor(strokeWidth + resolution * i),
-        //   Math.floor(strokeWidth + resolution * j),
-        //   resolution,
-        //   resolution
-        // );
+          ctx.fillRect(
+            Math.floor(strokeWidth + resolution * i),
+            Math.floor(strokeWidth + resolution * j),
+            resolution,
+            resolution
+          );
+          if (!showGrid) continue;
+          ctx.strokeRect(
+            Math.floor(strokeWidth + resolution * i),
+            Math.floor(strokeWidth + resolution * j),
+            resolution,
+            resolution
+          );
+        }
       }
     }
+    draw();
   }, [numCols, numRows, board, showGrid]);
 
   return (
@@ -53,28 +56,3 @@ export function Board({ board, showGrid }) {
     </>
   );
 }
-
-// function AnimatedCanvas() {
-//   const canvasRef = useRef(null);
-
-//   useEffect(() => {
-//     const canvas = canvasRef.current;
-//     const ctx = canvas.getContext("2d");
-//     let x = 0;
-//     let animationFrameId;
-
-//     const render = () => {
-//       ctx.clearRect(0, 0, canvas.width, canvas.height);
-//       ctx.fillStyle = "blue";
-//       ctx.fillRect(x, 50, 50, 50);
-//       x = (x + 2) % canvas.width;
-//       animationFrameId = requestAnimationFrame(render);
-//     };
-
-//     render();
-
-//     return () => cancelAnimationFrame(animationFrameId);
-//   }, []);
-
-//   return <canvas ref={canvasRef} width={400} height={200} style={{ border: "1px solid black" }} />;
-// }
--- src/components/GameOfLife.jsx
+++ src/components/GameOfLife.jsx
@@ -1,14 +1,71 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
+import { useImmer } from "use-immer";
 import { Board } from "./Board.jsx";
 
+const neighborPositions = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
 export function GameOfLife() {
-  const [showGrid, setShowGrid] = useState(true);
-  const [board, setBoard] = useState(() => createBoard(20, 20));
+  const [showGrid, setShowGrid] = useState(false);
+  const [speed, setSpeed] = useState(10);
+  const numRows = 150;
+  const numCols = 150;
+  const [board, setBoard] = useImmer(() => createBoard(numRows, numCols));
+
   function createBoard(numRows, numCols) {
     return Array.from({ length: numRows }, () =>
-      new Array(numCols).fill(Math.round(Math.random()))
+      Array.from({ length: numCols }, () => Math.round(Math.random()))
     );
   }
+
+  const updateBoard = useCallback(
+    (board) => {
+      setBoard((draft) => {
+        for (let i = 0; i < numCols; i++) {
+          for (let j = 0; j < numCols; j++) {
+            let numNeighbors = 0;
+            for (const [x, y] of neighborPositions) {
+              if (
+                i + x > 0 &&
+                i + x < numCols &&
+                j + y > 0 &&
+                j + y < numRows
+              ) {
+                numNeighbors += board[i + x][j + y];
+              }
+            }
+            if (board[i][j] === 1) {
+              if (numNeighbors > 3 || numNeighbors < 2) {
+                draft[i][j] = 0;
+              }
+            }
+            if (board[i][j] === 0) {
+              if (numNeighbors === 3) {
+                draft[i][j] = 1;
+              }
+            }
+          }
+        }
+        return draft;
+      });
+    },
+    [setBoard]
+  );
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      updateBoard(board);
+    }, speed);
+    return () => clearInterval(intervalId);
+  }, [speed, updateBoard, board]);
 
   return (
     <>
